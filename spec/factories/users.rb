5a79dbccfca9592bd791a8d9e9b62490d67bcc39
Igor Melão <igormelao@gmail.com>
2017-09-20 22:44:49 -0300

    improvment test for user association with team

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,11 @@ FactoryGirl.define do
     name { FFaker::Name.name }
     email { FFaker::Internet.email }
     password { FFaker::InternetSE.password }
+
+    factory :user_with_teams do
+      after(:create) do |user|
+        create(:team, user: user)
+      end
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,24 +4,32 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryGirl.create(:user)
   end
+  describe "creation" do
+    it "is valid with valid attributes" do
+      expect(@user).to be_valid
+    end
 
-  it "is valid with valid attributes" do
-    expect(@user).to be_valid
+    it "is not valid without name" do
+      @user.name = nil
+      expect(@user).not_to be_valid
+    end
+
+    it "is not valid without email" do
+      @user.email = nil
+      expect(@user).not_to be_valid
+    end
+
+    it "is not valid without password" do
+      @user.password = nil
+      expect(@user).not_to be_valid
+    end
   end
-
-  it "is not valid without name" do
-    @user.name = nil
-    expect(@user).not_to be_valid
-  end
-
-  it "is not valid without email" do
-    @user.email = nil
-    expect(@user).not_to be_valid
-  end
-
-  it "is not valid without password" do
-    @user.password = nil
-    expect(@user).not_to be_valid
+  
+  describe "associations" do
+    it "has one or more teams" do
+      @user_with_teams = FactoryGirl.create(:user_with_teams)
+      expect(@user_with_teams.teams.count).to eq(1)
+    end
   end
 
 end
