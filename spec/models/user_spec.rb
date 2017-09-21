6281ba18e2bd8beb7cfe60d4e3f104471d1de6a4
Igor Melão <igormelao@gmail.com>
2017-09-20 22:21:47 -0300

    creating models tests for user and improvment validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryGirl.create(:user)
+  end
+
+  it "is valid with valid attributes" do
+    expect(@user).to be_valid
+  end
+
+  it "is not valid without name" do
+    @user.name = nil
+    expect(@user).not_to be_valid
+  end
+
+  it "is not valid without email" do
+    @user.email = nil
+    expect(@user).not_to be_valid
+  end
+
+  it "is not valid without password" do
+    @user.password = nil
+    expect(@user).not_to be_valid
+  end
+
 end
